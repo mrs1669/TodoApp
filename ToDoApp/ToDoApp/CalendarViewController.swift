ff5f65295186a02f122e610ce425df4848e80e4e
工藤海斗 <kaitokudou2468110921@gmail.com>
2020-04-20 00:21:51 +0900

    カレンダーのヘッダーを日本語表示にした

--- ToDoApp/ToDoApp/CalendarViewController.swift
+++ ToDoApp/ToDoApp/CalendarViewController.swift
@@ -7,24 +7,45 @@
 //
 
 import UIKit
+import FSCalendar
+import CalculateCalendarLogic
 
-class CalendarViewController: UIViewController {
-
+class CalendarViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
+    
+    @IBOutlet weak var calendar: FSCalendar!
+    let calendarViewModel = CalendarViewModel()
+    let dayOfTheWeeks = ["日":0, "月":1, "火":2, "水":3, "木":4, "金":5, "土":6]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        calendar.delegate = self
+        calendar.dataSource = self
+        calendar.appearance.headerDateFormat = "yyyy年MM月"
+        
+        for (key, value) in self.dayOfTheWeeks {
+            self.calendar.calendarWeekdayView.weekdayLabels[value].text = key
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // 土日・祝日の文字色を変える
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleDefaultColorFor date: Date) -> UIColor? {
+        //祝日判定をする（祝日は赤色で表示する）
+        if self.calendarViewModel.judgeHoliday(date: date){
+            return UIColor.red
+        }
+        //土日の判定を行う（土曜日は青色、日曜日は赤色で表示する）
+        let weekDay = self.calendarViewModel.getWeekIndex(date: date)
+        if weekDay == 1 { // 日曜日
+            return UIColor.red
+        }
+        else if weekDay == 7 { // 土曜日
+            return UIColor.blue
+        }
+        
+        return nil
     }
-    */
-
+    
+    
 }
--- ToDoApp/ToDoApp/CalendarViewModel.swift
+++ ToDoApp/ToDoApp/CalendarViewModel.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import FSCalendar
+import CalculateCalendarLogic
+
+class CalendarViewModel {
+    // 祝日判定を行い結果を返すメソッド(True:祝日)
+    func judgeHoliday(date : Date) -> Bool{
+        //祝日判定用のカレンダークラスのインスタンス
+        let tmpCalendar = Calendar(identifier: .gregorian)
+        
+         // 祝日判定を行う日にちの年、月、日を取得
+        let year = tmpCalendar.component(.year, from: date)
+        let month = tmpCalendar.component(.month, from: date)
+        let day = tmpCalendar.component(.day, from: date)
+        
+        // CalculateCalendarLogic()：祝日判定のインスタンスの生成
+        let holiday = CalculateCalendarLogic()
+        
+        return holiday.judgeJapaneseHoliday(year: year, month: month, day: day)
+    }
+    
+    // 曜日判定(日曜日:1 〜 土曜日:7)
+    func getWeekIndex(date : Date) -> Int{
+        let tmpCalendar = Calendar(identifier: .gregorian)
+        return tmpCalendar.component(.weekday, from: date)
+    }
+}
