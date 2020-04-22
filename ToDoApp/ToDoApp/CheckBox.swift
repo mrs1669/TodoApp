743db4c68ccc0c47812e9db32086113f0081d829
工藤海斗 <kaitokudou2468110921@gmail.com>
2020-04-23 01:05:50 +0900

    チェックボックスとなる新しい画像をAssets.xcassetsに置いた

---
+++ ToDoApp/ToDoApp/CheckBox.swift
@@ -0,0 +1,21 @@
+//
+//  CheckBox.swift
+//  ToDoApp
+//
+//  Created by 工藤海斗 on 2020/04/22.
+//  Copyright © 2020 工藤海斗. All rights reserved.
+//
+
+import UIKit
+
+class CheckBox: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ToDoApp/ToDoApp/CustomToDoCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomToDoCell.swift
+//  ToDoApp
+//
+//  Created by 工藤海斗 on 2020/04/23.
+//  Copyright © 2020 工藤海斗. All rights reserved.
+//
+
+import UIKit
+
+class CustomToDoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
